f1689e3b0a8eb5c85351b9cd0d55b64dc396d1c8
Vanishar9880 <vanisharathore3019@gmail.com>
2024-08-25 19:18:11 +0530

    First Commit

---
+++ src/api.js
@@ -0,0 +1,17 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "406754da5emsh6575f54b6329935p1e6a7bjsnf9266b2c01d2",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "e480743b5c808ed3293e226afadeec2d"
+
+
+
